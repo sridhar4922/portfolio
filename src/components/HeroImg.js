554be255f699f0f830506bc6af84e117e11e185f
Sridhar S <srisridhar2002@gmail.com>
2024-02-15 00:14:54 +0530

    new

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -14,8 +14,7 @@ const HeroImg = () => {
           <h1>Sridhar S</h1>
           <h4>Full Stack Developer</h4>
           <div>
-            <Link to= "https://sridhar4922.github.io/Resume/" target="_blank" className="btn" >Resume</Link>
-            
+            <Link to= "https://sridhar4922.github.io/Resume/"  target="_blank" className="btn" >Resume</Link>
             <Link to="/contact" className="btn btn-light">Contact</Link>
           </div>
         </div>
